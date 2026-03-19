e78063ae22d4e450a8d147c0cd99818d4fb6e10f
maidinhthaison <mdts1988@gmail.com>
2026-03-19 17:35:02 +0700

    implement checkboxes

--- app/src/main/java/com/demo/jetpack_compose/fundamental_concepts/checkboxes/CheckboxesExample.kt
+++ app/src/main/java/com/demo/jetpack_compose/fundamental_concepts/checkboxes/CheckboxesExample.kt
@@ -1,2 +1,75 @@
 package com.demo.jetpack_compose.fundamental_concepts.checkboxes
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Text
+import androidx.compose.material3.TriStateCheckbox
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.state.ToggleableState
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CheckBoxExample(){
+    // Use mutableStateListOf to ensure recomposition
+    val childCheckedStates = remember { mutableStateListOf(false, false, false) }
+
+    val parentState = when {
+        childCheckedStates.all { it } -> ToggleableState.On
+        childCheckedStates.none { it } -> ToggleableState.Off
+        else -> ToggleableState.Indeterminate
+
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("Select all")
+            TriStateCheckbox(
+                state = parentState,
+                onClick ={
+                    val newState = parentState != ToggleableState.On
+                    for (i in childCheckedStates.indices) {
+                        childCheckedStates[i] = newState
+                    }
+                }
+            )
+
+        }
+
+        childCheckedStates.forEachIndexed { index, checked ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("Option ${index + 1}")
+                Checkbox(
+                    checked = checked,
+                    onCheckedChange = {
+                        childCheckedStates[index] = it
+                    }
+                )
+            }
+        }
+
+        if(childCheckedStates.all { it }){
+            Text("All options selected")
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun CheckBoxExamplePreview(){
+    CheckBoxExample()
+}
